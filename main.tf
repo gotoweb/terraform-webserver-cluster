4eec6abc2c16f5a12164fb3d138524e0bdac741c
Hoyong Lee <igotoweb@gmail.com>
2021-12-01 17:20:49 +0900

    특정 vpc에 적용 및 rds 인스턴스 추가

--- main.tf
+++ main.tf
@@ -12,6 +12,28 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+
+  azs             = ["ap-northeast-2a", "ap-northeast-2b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  one_nat_gateway_per_az = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-0252a84eb1d66c2a0"
   instance_type   = "t2.micro"
@@ -32,7 +54,8 @@ resource "aws_launch_configuration" "example" {
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+  vpc_zone_identifier  = module.vpc.public_subnets
 
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
@@ -49,6 +72,7 @@ resource "aws_autoscaling_group" "example" {
 
 resource "aws_security_group" "instance" {
   name = var.instance_security_group_name
+  vpc_id      = module.vpc.vpc_id
 
   ingress {
     from_port   = var.server_port
@@ -58,20 +82,12 @@ resource "aws_security_group" "instance" {
   }
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
-}
-
 resource "aws_lb" "example" {
 
   name               = var.alb_name
 
   load_balancer_type = "application"
-  subnets            = data.aws_subnet_ids.default.ids
+  subnets            = module.vpc.public_subnets
   security_groups    = [aws_security_group.alb.id]
 }
 
@@ -92,12 +108,13 @@ resource "aws_lb_target_group" "asg" {
 
   port     = var.server_port
   protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
+  vpc_id   = module.vpc.vpc_id
 }
 
 resource "aws_security_group" "alb" {
 
   name = var.alb_security_group_name
+  vpc_id      = module.vpc.vpc_id
 
   # Allow inbound HTTP requests
   ingress {
@@ -116,3 +133,23 @@ resource "aws_security_group" "alb" {
   }
 }
 
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_db_subnet_group.selected.name
+}
+
+resource "aws_db_subnet_group" "selected" {
+  name       = "main"
+  subnet_ids = module.vpc.private_subnets
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "alb_dns_name" {
   value       = aws_lb.example.dns_name
   description = "The domain name of the load balancer"
 }
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
